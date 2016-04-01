e5898a06a13de1ff4ebb1d04c6c119a09d3864bc
sngjss <sunga.jss@gmail.com>
2016-04-01 09:35:31 -0400

    Took out <h1>

--- page-templates/home.php
+++ page-templates/home.php
@@ -7,14 +7,10 @@ get_header(); ?>
 <?php get_template_part( 'template-parts/featured-image' ); ?>
 
 <div id="page-full-width" role="main">
-
-<h1> HOME TEST </h1>
-
 <?php do_action( 'foundationpress_before_content' ); ?>
 <?php while ( have_posts() ) : the_post(); ?>
   <article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
       <header>
-          <h1 class="entry-title"><?php the_title(); ?></h1>
       </header>
       <?php do_action( 'foundationpress_page_before_entry_content' ); ?>
       <div class="entry-content">
